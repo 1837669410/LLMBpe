f319dfd60af613cc59a2758b2344fba1cc1301f6
1837669410 <1837669410@qq.com>
2024-08-12 18:41:46 +0800

    修改测试文件

--- test.py
+++ test.py
@@ -1,21 +1,26 @@
+import os
+
 from Bpe import RegexTokenizer
 
 
 # 1、实例化tokenizer
-tokenizer = RegexTokenizer()
+tokenizer = RegexTokenizer(dropout=None)
 
 # 2、导入文本数据
-with open("D:\code\\textcrawl\datasets\gzdqy\\1.txt", "r", encoding="utf-8") as f:
-    text = f.read().strip()
+text = ""
+name_list = os.listdir("D:\code\\textcrawl\datasets\h_hub\gzdqy")
+for name in name_list:
+    with open(f"D:\code\\textcrawl\datasets\h_hub\gzdqy\\{name}", "r", encoding="utf-8") as f:
+        text += f.read().strip() + "\n"
 
 # 3、训练tokenizer
-tokenizer.train(text, vocab_size=300, verbose=True)
+tokenizer.train(text, vocab_size=1000, verbose=True)
 
 # 4、注册special_tokens
 tokenizer.register_special_tokens({
-    "<|startoftext|>": 300,
-    "<|endoftext|>": 301,
-    "<|padding|>": 302
+    "<|startoftext|>": 1000,
+    "<|endoftext|>": 1001,
+    "<|padding|>": 1002
 })
 print(tokenizer.merges)
 print(tokenizer.vocab)
@@ -36,9 +41,10 @@ print(tokenizer.pattern)
 print("===================================\n")
 
 # 7、编码
-with open("D:\code\\textcrawl\datasets\gnszjlb\\3.txt", "r", encoding="utf-8") as f:
+with open("D:\code\\textcrawl\datasets\h_hub\gzdqy\\3.txt", "r", encoding="utf-8") as f:
     en_text = "<|startoftext|>" + f.read().strip() + "<|endoftext|>" + "<|padding|>" + "<|padding|>"
 ids = tokenizer.encode(en_text)
+print(len(en_text))
 print(len(ids))
 
 # 8、解码
